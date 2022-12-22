1b3fb89c0cbc33f7bd8d55d5bf8e77ec14d728b9
gulera-israilova <70669975+gulera-israilova@users.noreply.github.com>
2022-12-22 15:04:11 +0600

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -18,17 +18,15 @@ config()
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: 'postgres://qwfvnkswfawvsx:88877d9f70258b24dd06287f35ee78103114df0eb51981a7194d3b435778e50d@ec2-34-230-110-100.compute-1.amazonaws.com:5432/dol5339buk6fr',
-      extra:{
+      "host": process.env.DB_HOST,
+      "port": +process.env.DB_PORT,
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
         ssl:{
           rejectUnauthorized:false
         }
       },
-      // "host": process.env.DB_HOST,
-      // "port": +process.env.DB_PORT,
-      // "username": process.env.DB_USERNAME,
-      // "password": process.env.DB_PASSWORD,
-      // "database": process.env.DB_DATABASE,
       entities:[UserEntity,BranchEntity,FlowerEntity,BouquetEntity,CategoryEntity,FlowerBouquetEntity],
       autoLoadEntities: true,
       synchronize: true,
